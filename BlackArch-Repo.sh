a94338bb5bd9e299548c35b3a4c714994e3e2f17
IAmBlackHacker <90060428+IAMMADHACKER@users.noreply.github.com>
2023-03-26 11:45:42 +0530

    Update BlackArch-Repo.sh

--- BlackArch-Repo.sh
+++ BlackArch-Repo.sh
@@ -11,4 +11,21 @@ clear
 toilet BlackArch-Repo | lolcat 
 cho" ~~~~~~~~~~~~~~> Created By IAMMADHACKER <~~~~~~~~~~~~ " | lolcat  
 sudo pacman -Syu
-sudo pacman -S Wget
+sudo pacman -S wget
+wget https://blackarch.org/strap.sh
+chmod +x strap.sh
+./strap.sh
+
+#After starp.sh file stops working 
+
+echo" BlackArch Repository Sucessfully Added To System !!!! " | lolcat 
+
+
+toilet Updating System | lolcat 
+sudo pacman -Syu
+
+#After sudo pacman -Syu command Stops working 
+
+echo" System sucessfully Upgraded 
+    Now you Can Use Black Arch Reposirtory With 2700+ Tools " | lolcat 
+
