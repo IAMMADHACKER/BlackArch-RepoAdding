2148cc7026ac03f805d5cf886925df19e694d18d
IAmBlackHacker <90060428+IAMMADHACKER@users.noreply.github.com>
2023-03-26 11:36:29 +0530

    Create BlackArch-Repo.sh

---
+++ BlackArch-Repo.sh
@@ -0,0 +1,14 @@
+clear
+
+cd /Downloads
+sudo pacman -Syu
+sudo pacman -S python ruby python-pip toilet
+gem install lolcat
+pip install lolcat
+
+clear
+
+toilet BlackArch-Repo | lolcat 
+cho" ~~~~~~~~~~~~~~> Created By IAMMADHACKER <~~~~~~~~~~~~ " | lolcat  
+sudo pacman -Syu
+sudo pacman -S Wget
